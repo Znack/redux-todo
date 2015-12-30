20a145ff88dc4e53969d631d8035e6caa0b2b7eb
znack <scher56@gmail.com>
2015-12-30 22:18:43 +0600

    initial commit

---
+++ src/redux/modules/todo.js
@@ -0,0 +1,60 @@
+import { createAction, handleActions } from 'redux-actions'
+
+// ------------------------------------
+// Constants
+// ------------------------------------
+export const COUNTER_INCREMENT = 'COUNTER_INCREMENT'
+
+export const ADD_TODO = 'ADD_TODO'
+export const COMPLETE_TODO = 'COMPLETE_TODO'
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+export const VisibilityFilters = {
+  SHOW_ALL: 'SHOW_ALL',
+  SHOW_COMPLETED: 'SHOW_COMPLETED',
+  SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
+
+// ------------------------------------
+// Actions
+// ------------------------------------
+
+export const actions = {
+  addTodo: createAction(ADD_TODO, (text) => text),
+  completeTodo: createAction(COMPLETE_TODO, (index) => index),
+  setVisibilityFilter: createAction(SET_VISIBILITY_FILTER, (value) => value)
+}
+
+// ------------------------------------
+// Reducer
+// ------------------------------------
+export const addTodo = (state, { payload }) => Object.assign({}, state, {todos: [...state.todos, {text: payload, completed: false}]})
+
+
+export const completeTodo = (state, { payload }) => {
+  return Object.assign({}, state, {todos: [
+    ...state.todos.slice(0, payload),
+    Object.assign({}, state.todos[payload], {completed: true}),
+    ...state.todos.slice(payload + 1)
+  ]})
+}
+
+export const setVisibilityFilter = (state, { payload }) => {
+  return Object.assign({}, state, {visibilityFilter: payload})
+}
+
+export default handleActions({
+  [ADD_TODO]: addTodo,
+  [COMPLETE_TODO]: completeTodo,
+  [SET_VISIBILITY_FILTER]: setVisibilityFilter
+}, {
+  visibilityFilter: 'SHOW_ALL',
+  todos: [{
+    text: 'Complete me!',
+    completed: false
+  }],
+  visibleTodos: [{
+    text: 'Complete me!',
+    completed: false
+  }]
+})
---
+++ src/views/TodoView.js
@@ -0,0 +1,68 @@
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
+import { actions, VisibilityFilters } from '../redux/modules/todo'
+import AddTodo from '../components/todo/AddTodo'
+import TodoList from '../components/todo/TodoList'
+import Footer from '../components/todo/Footer'
+
+const selectTodos = (todos, filter) => {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed)
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log('state', state)
+  return {
+    todos: state.todo.todos,
+    visibleTodos: selectTodos(state.todo.todos, state.todo.visibilityFilter),
+    visibilityFilter: state.todo.visibilityFilter
+  }
+}
+
+class TodoView extends React.Component {
+  static propTypes = {
+    todo: React.PropTypes.shape({
+      visibleTodos: React.PropTypes.arrayOf(React.PropTypes.shape({
+        text: React.PropTypes.string.isRequired,
+        completed: React.PropTypes.bool.isRequired
+      })),
+      visibilityFilter: React.PropTypes.oneOf([
+        'SHOW_ALL',
+        'SHOW_COMPLETED',
+        'SHOW_ACTIVE'
+      ]).isRequired,
+    }),
+    addTodo: React.PropTypes.func.isRequired,
+    completeTodo: React.PropTypes.func.isRequired,
+    setVisibilityFilter: React.PropTypes.func.isRequired
+  }
+
+  render () {
+    console.log(this.props)
+    return (
+
+      <div className='container text-center'>
+        <h1>React Redux Starter Kit's Todo</h1>
+        <AddTodo
+          onAddClick={this.props.addTodo} />
+        <TodoList
+          todos={this.props.visibleTodos}
+          onTodoClick={this.props.completeTodo} />
+        <Footer
+          filter={this.props.visibilityFilter}
+          onFilterChange={this.props.setVisibilityFilter} />
+        <hr />
+        <Link to='/'>Go To Home View</Link>
+        <Link to='/about'>Go To About View</Link>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps, actions)(TodoView)
